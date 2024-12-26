8e50673cd64f15050385eb135850b4491b60d399
apple <apple@apples-MacBook-Pro.local>
2024-12-26 12:36:24 +0100

    work finished

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import { useState } from 'react';
 
 function App() {
   const [visible, setVisible] = useState(false)
-  const mediaQuery = window.matchMedia("(max-width: 768px)");
- const getFollow= document.getElementById('followKey')
-const getArrow= document.querySelector(".arrow")
- const getClick=()=>{
+  const [theDisplay, setTheDisplay]=useState(null)
+  const [theArrow, setTheArrow]=useState(null)
  
+
+ const getClick=()=>{
+
   if(!visible){
-      getFollow.style.display="inline-block"
-      getArrow.style.display="inline-block"
-      setVisible(true)
-  } else if(visible){
-      getFollow.style.display="none"
-      getArrow.style.display="none"
+    setTheDisplay(true)
+    setTheArrow(true)
+    setVisible(true)
+     
+    console.log(visible)
+ 
+  } else{
+    
+      setTheDisplay (false)
+      setTheArrow(false)
       setVisible(false)
+       
+      console.log(visible)
   }
+
+ }
+ const toggleDisplay =()=>{
 
  }
   return (
@@ -34,14 +44,14 @@ const getArrow= document.querySelector(".arrow")
         Ever been in a room and felt like something was missing? Perhaps it felt slightly 
         bare and uninviting. I've got some tips to help you make any room feel complete.
       </p>
-      <div className='follow' id='followKey'>
+      <div className='follow' id='followKey' style={{ display:  theDisplay ? 'inline-block' : 'none' }}>
     
    <span className='follow-span'>SHARE</span>   <img src='images/icon-facebook.svg' alt='facebook' />
      <img src='images/icon-twitter.svg' alt='twitter' />
      <img src='images/icon-pinterest.svg' alt='pinterest' />
 
     </div>
-    <div className='arrow'></div>
+    <div className='arrow' style={{ display:  theArrow ? 'inline-block' : 'none' }}></div>
       <div className='footer'>
     <div className='item1'>
       <img src='images/avatar-michelle.jpg' />
